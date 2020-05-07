23224a095c36d32e226df0b2d7d276f00d8b7e3b
kaleyfawcett <kaley.fawcett@gmail.com>
2020-05-06 22:33:24 -0400

    everything is there but little styling done

--- main.js
+++ main.js
@@ -1,15 +1,58 @@
-function renderPageContent () {
+let dataSection = document.querySelector("#bio")
 
-}
+fetch('https://api.github.com/users/kaleyfawcett')
+    .then (function (response) {
+        return response.json()
+    })
+
+    .then (function (data) {
+
+        let h1Element = document.createElement ('div')
+        h1Element.innerText = `Name: ${data.name}` 
+        dataSection.appendChild(h1Element)
+        return data.url
+        
+    }) 
+
+    .then (url => fetch (url))
+    .then (response => response.json())
+    .then (function (dataPhoto) {
+
+        let photoElement = document.createElement ('div')
+        photoElement.innerHTML = `<img src= ${dataPhoto.avatar_url}>`
+        dataSection.appendChild(photoElement)
+        return dataPhoto.url
 
 
+    })
 
+    .then (url => fetch (url))
+    .then (response => response.json())
+    .then (function (gitUser) {
 
+        let gitHubElement = document.createElement ('div')
+        gitHubElement.innerHTML = `GitHub URL: ${gitUser.login}`
+        dataSection.appendChild(gitHubElement)
+        return gitUser.url
+    })
 
+    .then (url => fetch (url))
+    .then (response => response.json())
+    .then (function (company) {
 
+        let companyElement = document.createElement ('div')
+        companyElement.innerHTML = `Company: ${company.company}` 
+        dataSection.appendChild(companyElement)
+        return company.url
+    })
 
+    .then (url => fetch (url))
+    .then (response => response.json())
+    .then (function (website) {
 
+        let websiteElement = document.createElement ('div')
+        websiteElement.innerHTML = website.blog
+        dataSection.appendChild(websiteElement)
+        return website.url
+    })
 
-
-
-renderPageContent ()
